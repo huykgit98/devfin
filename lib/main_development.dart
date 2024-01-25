777210775882ef492b401b5868469975f510cf55
Negan <huykgit98@gmail.com>
2024-01-25 16:28:16 +0700

    fix iOS 17 build issues

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,11 +1,11 @@
 import 'package:devfin/app/app.dart';
 import 'package:devfin/bootstrap.dart';
-import 'package:devfin/firebase_options_dev.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:devfin/firebase_options_dev.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/cupertino.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await bootstrap(() => const DevFinApp());
 }
